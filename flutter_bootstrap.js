95e8c2ad809faadaba01a47584af11c15f5bd516
kkrzeczk <kinga.krzeczkowska@gmail.com>
2026-06-21 20:49:20 +0200

    Poprawki do aplikacji

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2445926370" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "732741095" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
